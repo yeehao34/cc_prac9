b181ce096d418ca84016e19d3060326f23a71540
New Yee Hao <88917687+yeehao34@users.noreply.github.com>
2023-09-03 22:52:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c1nlreg7eojl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "newyeehao-employee"
+customregion = "us-east-1"
+
